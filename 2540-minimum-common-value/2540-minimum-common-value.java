040e66ce8c3c90394aade32f88300ded87acd687
Madhusudhan Kosari <23211a05e8@bvrit.ac.in>
2026-05-19 13:43:46 +0530

    Time: 6 ms (28.46%), Space: 80.5 MB (48.43%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,11 +1,35 @@
 class Solution {
-    public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> hs = new HashSet<>();
-        for(int num : nums1){
-            hs.add(num);
+    public boolean binarySearch(int arr[],int k){
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] == k){
+                return true;
+            }
+            else if(arr[mid]< k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
         }
-        for(int num : nums2){
-            if(hs.contains(num)){
+        return false;
+    }
+    public int getCommon(int[] nums1, int[] nums2) {
+        // HashSet<Integer> hs = new HashSet<>();
+        // for(int num : nums1){
+        //     hs.add(num);
+        // }
+        // for(int num : nums2){
+        //     if(hs.contains(num)){
+        //         return num;
+        //     }
+        // }
+        // return -1;
+        // boolean visit = false;
+        for(int num : nums1){
+            if(binarySearch(nums2,num)){
                 return num;
             }
         }
