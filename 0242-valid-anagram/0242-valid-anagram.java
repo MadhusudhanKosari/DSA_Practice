bfd1a3bab0d84a11c4fa4cef1fbd66dd96c69199
Madhusudhan Kosari <23211a05e8@bvrit.ac.in>
2026-06-19 01:07:08 +0530

    Time: 2 ms (99.59%), Space: 44.5 MB (66.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int arr[] = new int[26];
-        if(s.length() != t.length()) return false;
+        int frq[] = new int[26];
         for(char ch : s.toCharArray()){
-            arr[ch-'a']++;
+            frq[ch-'a']++;
         }
         for(char ch : t.toCharArray()){
-            arr[ch - 'a']--;
+            frq[ch-'a']--;
         }
-        for(int num : arr){
-            if(num != 0){
-                return false;
-            }
+        for(int i = 0;i<26;i++){
+            if(frq[i] != 0) return false;
         }
         return true;
     }
